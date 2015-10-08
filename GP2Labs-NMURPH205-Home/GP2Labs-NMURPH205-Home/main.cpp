1db6724e744c153aaec1637a6db27e439c018fbe
Nick047 <nicomurphy@live.co.uk>
2015-10-08 13:12:33 +0100

    OpenGL linked
    Lab 1 8.7 finished, opengl additional dependants added and initial
    opengl states set

--- GP2Labs-NMURPH205-Home/GP2Labs-NMURPH205-Home/main.cpp
+++ GP2Labs-NMURPH205-Home/GP2Labs-NMURPH205-Home/main.cpp
@@ -3,6 +3,10 @@
 #include <iostream>	//this allows information to be output which is useful for debugging
 //Header for SDL2 functionality
 #include <SDL.h>
+//opengl header files
+#include <SDL_opengl.h>
+#include <gl\GLU.h>
+
 
 //Global variables
 //pointers to out sdl windows
@@ -12,6 +16,9 @@ SDL_Window * window;
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
 bool running = true;	//set to false when you want teh game to stop runinng
+//SDL GL Context
+SDL_GLContext glcontext = NULL;
+
 
 //Global Functions
 void InitWindow(int width, int height, bool fullscreen)	//function takes in 2 integers and a boolean fullscreen or not
@@ -25,10 +32,77 @@ void InitWindow(int width, int height, bool fullscreen)	//function takes in 2 in
 
 void CleanUp()	//clears the meemory when exiting the game loop
 {
+	SDL_GL_DeleteContext(glcontext);
+
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 }
 
+//Function to initialise OpenGL
+void initOpenGL()
+{
+	//Create OpenGL Context
+	glcontext = SDL_GL_CreateContext(window);
+	//Something is wrong in creating the context, if it is still NULL
+	if(!glcontext)
+	{
+		std::cout << "Error creating OpenGL Context" << SDL_GetError() <<
+			std:: endl;
+	}
+
+	//Smooth Shading
+	glShadeModel(GL_SMOOTH);
+
+	//clear the background to black
+	glClearColor( 0.0f, 0.0f, 0.0f, 0.0f);
+
+	//Clear the depth buffer to 1.0
+	glClearDepth(1.0f);
+
+	//Enable depth testing
+	glEnable(GL_DEPTH_TEST);
+
+	//The depth to use
+	glDepthFunc(GL_LEQUAL);
+
+	//Turn on perspective correction
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+}
+
+//Function to set/reset viewport
+void setViewport( int width, int height)
+{
+	//screen ratio
+	GLfloat ratio;
+
+	//make sure height is always above 0
+	if (height == 0)
+	{
+		height =1;
+	}
+
+	//calculate screen ratio
+	ratio = (GLfloat)width / (GLfloat)height;
+
+	//Setup viewport
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+	//this code is supposed to be after the call to glViewport but all I can see is the setup might be me being stupid but i'll put the code here
+
+	//Change to project matrix mode
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	//Calculate perspective matrix, using glu library functions
+	gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+
+	//Switch to ModelView
+	glMatrixMode(GL_MODELVIEW);
+
+	//Reset using the identity matrix
+	glLoadIdentity();
+
+}
 
 //Main method - entry point
 int main(int argc, char* args[])	//integer trcks cmdline arguments and a pointer to an array of character strings containing the argumants //the returned integer detern=mines whether it has been run successfully 0 == success or a negative if not
@@ -45,6 +119,11 @@ int main(int argc, char* args[])	//integer trcks cmdline arguments and a pointer
 //Last year I had this problem it is a result of compatability with your mouse of monitor changing these should solve the problem though changing monitors is signficantly more annoying
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false);
 
+	//Call out InitOpenGL Function
+	initOpenGL();
+	//Set our viewport
+	setViewport(WINDOW_WIDTH, WINDOW_HEIGHT);
+
 	SDL_Event event;
 	while(running)
 	{
@@ -54,7 +133,7 @@ int main(int argc, char* args[])	//integer trcks cmdline arguments and a pointer
 			if(event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
 			{
 				//set our boolean which controls game loop to false
-				running = false;
+				running = false;		//can't use two == because 
 			}
 		}
 	}
